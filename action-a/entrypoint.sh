d9a29b506ba4c464849a3dba88212a5ae027482a
Arvind Krishna <54891738+ArvindAROO@users.noreply.github.com>
2020-08-16 19:27:58 +0530

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $INPUT_MY_NAME"
+sh -c "echo Hello world! My first workflow and continuous integration project learning from github education my name is $INPUT_MY_NAME"
